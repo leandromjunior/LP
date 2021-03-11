1dfa14873e7954c0bc8418da9bf783f23fc2900d
Leandro Motta Junior <lemottajr@hotmail.com>
2021-03-11 17:31:08 -0300

    Adicionando estrutura para nao repetir valores

--- vetor.pas
+++ vetor.pas
@@ -1,28 +1,46 @@
-
 program vetor;
 var 
     vet:array[0..9] of integer;
-    i, j,aux :integer;
+    i, j,aux, num :integer;
+    num_rep : boolean;
     
     //Função para printar o vetor
     procedure mostraVetor ( vetor : array of integer) ;
-        var i : integer;
+        var i : integer; //Local
         begin
             for i := 0 to 9 do
             write(vetor[i], ' ');
         end; // Fim 
-    
-begin // Inicio do main
+
+// Inicio do main 
+begin 
+
     // Preenchendo o vetor
     for i := 0 to 9 do
     begin
-
-        writeln('Informe o valor da posição: ', i);
-        read(vet[i]); //Lendo valores
+    
+        num_rep := true;
+        while num_rep do
+        begin
+            writeln('Informe o valor da posição: ', i);
+            read(vet[i]); //Lendo valores
+            num := vet[i];
+            num_rep := false;
+            
+            for j:=0 to (i-1) do
+            begin
+                if num = vet[j] then
+                begin
+                    writeln('Este valor ja existe. Informe outro: ');
+                    num_rep := true;
+                end;
+            end;
+        end;
     end;
     writeln(' ');
     mostraVetor(vet);
     
+    // Ordenando os valores em vet
     for i:= 0 to 9 do
     begin
         for j:=0 to 9 do
@@ -36,10 +54,12 @@ begin // Inicio do main
             end;
         end;
     end;
+    
     writeln;
     write('Vetor Organizado: ');
     mostraVetor(vet);
     writeln;
     writeln('Segundo maior numero do vetor: ', vet[8]);
+    
 end.
 
